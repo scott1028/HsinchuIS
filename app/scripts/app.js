fb04e2d1908e39137ebb2329458d9266f72bc2f7
scott.lan <scott.lan@taisys.com>
2014-06-07 20:20:07 +0800

    update first page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('HsinchuIS', ['ionic'])
+angular.module('HsinchuIS', ['ionic', 'ngRoute'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -12,9 +12,5 @@ angular.module('HsinchuIS', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
-.when('/main', {
-  templateUrl: 'views/main.html',
-  controller: 'MainCtrl'
 })
 ;
